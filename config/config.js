d618c7a774d7e6a69471f31c735fbe01a633ec23
JoacoVeron4 <joacoveron@gmail.com>
2021-10-15 12:07:40 -0300

    first commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+require("dotenv").config();
+dotenv.config({ path: "./config/config.env" });
+
+module.exports = {
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
---
+++ index.js
@@ -0,0 +1,42 @@
+const Twit = require("twit");
+const config = require("./config/config.js");
+
+const T = new Twit(config);
+const stream = T.stream("statuses/filter", { track: "#FriendsRandomEpisode" });
+const jsonfile = require("jsonfile");
+
+stream.on("tweet", response);
+
+function response(tweet) {
+  const id = tweet.id_str;
+  const name = tweet.user.screen_name;
+  const episode = randomEpisode();
+  const replyText = `@${name} Gracias por usarme! Ahora te toca viciar ${episode.title}. Es el numero ${episode.episode_in_season} de la temporada ${episode.season}. Disfrutalo!`;
+
+  T.post(
+    "statuses/update",
+    { status: replyText, in_reply_to_status_id: id },
+    gameOver
+  );
+
+  function gameOver(err, reply) {
+    if (err) {
+      console.log(err.message);
+      console.log("Game Over");
+    } else {
+      console.log("Tweeted: " + reply.text);
+    }
+  }
+}
+
+function randomEpisode() {
+  const database = jsonfile.readFileSync("episodes.json");
+  const randomNumber = Math.floor(Math.random() * 236);
+  const episode = database[randomNumber - 1];
+  return {
+    episode_number: episode.episode_number,
+    season: episode.season,
+    episode_in_season: episode.episode_in_season,
+    title: episode.title,
+  };
+}
